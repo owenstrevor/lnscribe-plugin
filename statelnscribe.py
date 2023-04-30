4ecdeb3271deabc16e3d83af645b805a1957af64
NickCalabs <calabro.nick@gmail.com>
2023-04-30 14:00:05 -0500

    add error exception

--- statelnscribe.py
+++ statelnscribe.py
@@ -13,21 +13,28 @@ creds = grpc.ssl_channel_credentials(cert)
 channel = grpc.secure_channel('localhost:10009', creds)
 stub = lnrpc.LightningStub(channel)
 
-# Call listchannels gRPC method
-response = stub.ListChannels(lnrpc.ListChannelsRequest())
+try:
+    # Call listchannels gRPC method
+    response = stub.ListChannels(lnrpc.ListChannelsRequest())
 
-# Print channel state and balance
-for channel in response.channels:
-    print("Channel ID:", codecs.encode(channel.chan_id, 'hex'))
-    print("Channel state:", channel.channel_status)
-    print("Local balance:", channel.local_balance)
-    print("Remote balance:", channel.remote_balance)
+    # Print channel state and balance
+    for channel in response.channels:
+        print("Channel ID:", codecs.encode(channel.chan_id, 'hex'))
+        print("Channel state:", channel.channel_status)
+        print("Local balance:", channel.local_balance)
+        print("Remote balance:", channel.remote_balance)
 
+    # Call exportchanbackup gRPC method
+    response = stub.ExportChannelBackup(lnrpc.ChanBackupExportRequest())
 
-# Call exportchanbackup gRPC method
-response = stub.ExportChannelBackup(lnrpc.ChanBackupExportRequest())
+    # Save channel backup to file
+    #TODO: update file name with channel ID & date
+    with open('channelstate.backup', 'wb') as f:
+        f.write(response.backup)
 
-# Save channel backup to file
-#TODO: update file name with channel ID & date
-with open('channelstate.backup', 'wb') as f:
-    f.write(response.backup)
+except grpc.RpcError as e:
+    print("Error occurred during gRPC call: ", e)
+
+finally:
+    # close gRPC channel
+    channel.close()
